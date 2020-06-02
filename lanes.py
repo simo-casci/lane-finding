a76d6424c766e0d86fe9d2e85cadf3da69362466
simo-casci <simone.cascino546@edu.unito.it>
2020-06-02 12:27:31 +0200

    initial commit

--- lanes.py
+++ lanes.py
@@ -71,7 +71,7 @@ combo_image = cv2.addWeighted(lane_image, 0.8, line_image, 1, 1)
 cv2.imshow('result', combo_image)
 cv2.waitKey(0)
 '''
-cap = cv2.VideoCapture('road-video/test2.mp4')
+cap = cv2.VideoCapture('test2.mp4')
 while cap.isOpened():
 	_, frame = cap.read()
 	canny_image = canny(frame)
